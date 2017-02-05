7b13b961b8bf820b3d89c78152e78e47dbd3d0ef
Bconstantlee <bconstantlee@gmail.com>
2017-02-05 12:12:26 -0500

    npm start modified

--- app.js
+++ app.js
@@ -2,16 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('port', (process.env.PORT || 3000));
-app.set('views', path.join(__dirname, '/'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+var port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URL, function(error){
   if (error)
       console.log(error);
@@ -19,22 +14,14 @@ mongoose.connect(process.env.MONGO_URL, function(error){
       console.log('mongo connected');
   }
 });
-app.get('/', function(req, res, next) {
-    res.render('index.html');
-});
+app.use('/', express.static(__dirname + '/'));
 
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
-const server = app.listen(3000, function(err) {
+
+const server = app.listen(port, function(err) {
   if (err) {
       console.log(err);
       return;
   }
-  console.log("Listening on port " + 3000);
+  console.log("Listening on port " + port);
 });
-
-module.exports = app;
