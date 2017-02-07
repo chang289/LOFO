c17bbec9f2a3326dbe43b0969a4c2b70ea3cc498
Bconstantlee <bconstantlee@gmail.com>
2017-02-07 13:15:54 -0500

    Post model and api updated

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ mongoose.connect(process.env.MONGO_URL, function(error){
 // routes(app);
 
 app.use('/', express.static(__dirname + '/'));
-app.post('/post', function (req, res){
+
+//create new post
+app.post('/post/create', function (req, res){
   var newPost = new Post(req.body);
   newPost.save((err)=>{
       if (err){
@@ -30,6 +32,66 @@ app.post('/post', function (req, res){
       res.json({info: 'Post created successfully', data: newPost});
   });
 });
+
+//display all ongoing post
+app.get('/post/get/ongoing', function(req, res) {
+  Post.find({ "complete": 0 })
+    .sort({ createTime: -1 , modifiedTime: -1 })
+    .exec(function(err, post){
+      if (err){
+          res.json({info: 'error', error: err});
+      }
+      if (post.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: post}); }
+    });
+});
+
+//display all ongoing post
+app.get('/post/get/complete', function(req, res) {
+  Post.find({ "complete": 1 })
+    .sort({ createTime: -1 , modifiedTime: -1 })
+    .exec(function(err, post){
+      if (err){
+          res.json({info: 'error', error: err});
+      }
+      if (post.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: post}); }
+    });
+});
+
+//get post by id
+app.get('/post/get/:id', function(req, res){
+  Post.findById(req.params.id, function (err, post) {
+    if(err)
+      res.json({info: 'error', error: err});
+    res.json({info: 'Post found', data: post});
+  });
+});
+
+//delete post by id
+app.delete('/post/delete/:id', function(req, res){
+  //remove post by ID
+  Post.remove({ _id: req.params.id }, function(err){
+    if(err)
+      res.json({info: 'error', error: err});
+    res.json({ message : 'Post delete'});
+  });
+});
+
+//edit post by id
+app.post('/post/edit/:id', function(req, res){
+  Post.findById(req.params.id, function (err, post) {
+    if(err)
+      res.json({info: 'error', error: err});
+    post = new Post(req.body);
+    post.save(function(err, po){
+      if(err)
+        res.json({info: 'error', error: err});
+      res.json({info: 'Posts updated', data: post});
+    });
+  });
+});
+
 
 const server = app.listen(port, function(err) {
   if (err) {
