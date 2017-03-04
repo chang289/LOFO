0998755e6165369734afb47150f0153bbee2713d
Frank Hu <hu353@purdue.edu>
2017-03-03 22:02:22 -0500

    update

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ mongoose.connect("mongodb://tester:abc123@ds021166.mlab.com:21166/playground", f
 // routes(app);
 
 app.use('/', express.static(__dirname + '/dist'));
+//create new post
+//create new post
+app.use('/', express.static(__dirname + '/'));
 
 //create new post
 app.post('/post/create', function (req, res){
@@ -37,7 +40,7 @@ app.post('/post/create', function (req, res){
 //display all ongoing post
 app.get('/post/get/ongoing', function(req, res) {
   Post.find({ "complete": 0 })
-    .sort({ createTime: -1 , modifiedTime: -1 })
+    .sort({ modifiedTime: -1 })
     .exec(function(err, post){
       if (err){
           return res.json({info: 'error', error: err});
@@ -47,10 +50,10 @@ app.get('/post/get/ongoing', function(req, res) {
     });
 });
 
-//display all ongoing post
+//display all complete post
 app.get('/post/get/complete', function(req, res) {
   Post.find({ "complete": 1 })
-    .sort({ createTime: -1 , modifiedTime: -1 })
+    .sort({ modifiedTime: -1 })
     .exec(function(err, post){
       if (err){
           return res.json({info: 'error', error: err});
@@ -73,7 +76,9 @@ app.get('/post/get/:id', function(req, res){
 
 //get post by poster's email
 app.get('/post/get/email/:poster', function(req, res){
-  Post.find( {'poster': req.params.poster}, function (err, post) {
+  Post.find( {'poster': req.params.poster})
+  .sort({ modifiedTime: -1 })
+  .exec(function(err, post){
     if(err)
       return res.json({info: 'error', error: err});
     if (post.length == 0)
@@ -88,17 +93,17 @@ app.delete('/post/delete/:id', function(req, res){
   Post.remove({ _id: req.params.id }, function(err){
     if(err)
       return res.json({info: 'error', error: err});
-    res.json({ message : 'Post delete'});
+    res.json({ info : 'Post delete'});
   });
 });
 
 //delete all post with title contains "test"
 app.delete('/post/deleteAll', function(req, res){
   //remove post by ID
-  Post.remove({ "title" : {$regex : ".*Test.*"} }, function(err){
+  Post.remove({ "title" : {$regex : ".*test.*"} }, function(err){
     if(err)
       return res.json({info: 'error', error: err});
-    res.json({ message : 'Post delete'});
+    res.json({ info : 'Post delete'});
   });
 });
 
@@ -108,7 +113,7 @@ app.post('/post/edit/:id', function(req, res){
     if(err)
       return res.json({info: 'error', error: err});
     if(!post){
-      return res.json({ message : 'No such post'});
+      return res.json({ info : 'No such post'});
     }
     post.fullname = req.body.fullname;
     post.title = req.body.title;
@@ -117,6 +122,8 @@ app.post('/post/edit/:id', function(req, res){
     post.contact = req.body.contact;
     post.photo = req.body.photo;
     post.modifiedTime = new Date();
+    post.complete = req.body.complete;
+    post.confirmer = req.body.confirmer;
 
     post.save(function(err, po){
       if(err)
@@ -143,13 +150,166 @@ app.post('/user/login', function(req, res) {
     if(err)
       return res.json({info: 'error', error: err});
     if (!user) {
-      return res.json({ message : 'No such user'});
+      return res.json({ info : 'No such user'});
     }
     if (req.body.password == user.password) {
       res.json({info: 'Login successfully', data: user});
-    } else { res.json({ message : 'Password invalid'}); }
+    } else { res.json({ info : 'Password invalid'}); }
   })
 });
+
+//delete user by id
+app.delete('/user/delete/:id', function(req, res){
+  //remove user by ID
+  User.remove({ _id: req.params.id }, function(err){
+    if(err)
+      return res.json({info: 'error', error: err});
+    res.json({ info : 'User delete'});
+  });
+});
+
+// app.post('/image/upload', function(req, res) {
+//   // console.log(req.files);
+//   var file = req.files.file;
+//   var stream = fs.createReadStream(file.path);
+//   var uid = uuidV1() + "." + req.body.format;
+//   console.log(uid);
+//   s3fsImpl.writeFile(uid, stream).then(function(){
+//     // fs.unlink(req.body.path, function(err){
+//     //   if (err)
+//     //     console.log("Sending failed");
+//       return res.send('https://s3.amazonaws.com/lofo-purdue/' + uid);
+//     // });
+//   });
+// });
+
+//ascending
+app.get('/post/sort/:tag/:starterDate/:endDate/:lost/asc', function(req, res){
+
+  Post.find({
+    "tag": {"$eq": req.params.tag},
+    "lost": {"$eq": req.params.lost},
+    "modifiedTime": {$gte: req.params.starterDate, $lte: req.params.endDate},
+    "complete": 0
+  })
+  .sort({ modifiedTime: 1 })
+  .exec(function(err, posts){
+    if (err){
+        return res.json({info: 'error', error: err});
+    }
+    if (posts.length == 0) {res.json({info: 'No posts found'});}
+    else {res.json({info: 'Posts found', data: posts}); }
+  });
+});
+
+//descending
+app.get('/post/sort/:tag/:starterDate/:endDate/:lost/des', function(req, res){
+
+    Post.find({
+      "tag": {"$eq": req.params.tag},
+      "lost": {"$eq": req.params.lost},
+      "modifiedTime": {$gte: req.params.starterDate, $lte: req.params.endDate},
+      "complete": 0
+    })
+    .sort({ modifiedTime: -1 })
+    .exec(function(err, posts){
+      if (err){
+          return res.json({info: 'error', error: err});
+      }
+      if (posts.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: posts}); }
+    });
+});
+
+app.get('/post/sort/tag/:tag', function(req, res){
+    if (req.params.tag == -1) {
+      Post.find({ "complete": 0 })
+      .sort({ modifiedTime: -1 })
+      .exec(function(err, post){
+        if (err){
+            return res.json({info: 'error', error: err});
+        }
+        if (post.length == 0) {res.json({info: 'No posts found'});}
+        else {res.json({info: 'Posts found', data: post}); }
+      });
+    }
+    else {
+      Post.find({
+        "tag": {"$eq": req.params.tag},
+        "complete": 0
+      })
+      .sort({ modifiedTime: -1 })
+      .exec(function(err, posts){
+        if (err){
+            return res.json({info: 'error', error: err});
+        }
+        if (posts.length == 0) {res.json({info: 'No posts found'});}
+        else {res.json({info: 'Posts found', data: posts}); }
+      });
+    }
+});
+
+app.get('/post/sort/date/:starterDate/:endDate', function(req, res){
+  var start = new Date(req.params.starterDate);
+  var end = new Date(req.params.endDate);
+  end.setDate(end.getDate() + 1);
+
+  if (req.params.starterDate == "undefined" || req.params.endDate == "undefined") {
+    Post.find({ "complete": 0 })
+    .sort({ modifiedTime: -1 })
+    .exec(function(err, post){
+      if (err){
+          return res.json({info: 'error', error: err});
+      }
+      if (post.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: post}); }
+    });
+  }
+  else {
+    Post.find({
+      "modifiedTime": {$gte: start, $lte: end},
+      "complete": 0
+    })
+    .sort({ modifiedTime: -1 })
+    .exec(function(err, posts){
+
+      if (err){
+          return res.json({info: 'error', error: err});
+      }
+      if (posts.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: posts}); }
+    });
+  }
+});
+
+app.get('/post/sort/lost/:lost', function(req, res){
+  if (req.params.lost == "undefined") {
+    Post.find({ "complete": 0 })
+    .sort({ modifiedTime: -1 })
+    .exec(function(err, post){
+      if (err){
+          return res.json({info: 'error', error: err});
+      }
+      if (post.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: post}); }
+    });
+  }
+  else {
+    Post.find({
+      "lost": {"$eq": req.params.lost},
+      "complete": 0
+    })
+    .sort({ modifiedTime: -1 })
+    .exec(function(err, posts){
+      if (err){
+          return res.json({info: 'error', error: err});
+      }
+      if (posts.length == 0) {res.json({info: 'No posts found'});}
+      else {res.json({info: 'Posts found', data: posts}); }
+    });
+  }
+});
+
 
 const server = app.listen(port, function(err) {
   if (err) {
@@ -158,3 +318,5 @@ const server = app.listen(port, function(err) {
   }
   console.log("Listening on port " + port);
 });
+
+module.exports = app;
